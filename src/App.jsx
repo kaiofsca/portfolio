4075c02fed2e0590077be3229fbce250ffb29601
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-11-28 10:50:47 -0300

    criando os arquivos das seções

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import Navbar from './sections/navbar/Navbar'
+import Header from './sections/header/Header'
+import About from './sections/about/About'
+import Skills from './sections/skills/Skills'
+import Portfolio from './sections/portfolio/Portfolio'
+import Contact from './sections/contact/Contact'
+import Footer from './sections/footer/Footer'
+import FloatingNav from './sections/floating-nav/FloatingNav'
+
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <main>
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Portfolio />
+      <Contact />
+      <Footer />
+      <FloatingNav />
+    </main>
   )
 }
 
---
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -0,0 +1,9 @@
+import './floating-nav.css'
+
+const FloatingNav = () => {
+  return (
+    <section id='floatingNav'><h2>FloatingNav</h2></section>
+  )
+}
+
+export default FloatingNav
