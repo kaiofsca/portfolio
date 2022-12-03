555d256b435d7c62bb753f6de59391168a38d2f8
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-12-03 07:15:16 -0300

    criando e estilizando o header

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,31 @@
+import HeaderImage from '../../assets/avatar.png'
+import data from './data'
 import './header.css'
 
 const Header = () => {
   return (
-    <header id='header'><h2>Header</h2></header>
+    <header id='header'>
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Header Profile" />
+        </div>
+        <h3>Kaio Fonseca</h3>
+        <p>
+          Me preocupo com cada detalhe na hora de construir interfaces valorizando a experiência para que ela impacte positivamente a vida dos usuários!
+        </p>
+        <div className="header__cta">
+          <a href='#contact' className='btn primary'>Let's Talk</a>
+          <a href='#portfolio' className='btn light'>My work</a>
+        </div>
+        <div className="header__socials">
+          {
+            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">
+              {item.icon}
+            </a>)
+          }
+        </div>
+      </div>
+    </header>
   )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,11 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+
+const data = [
+    {id: 1, link: 'https://www.instagram.com/dev.kaiof/', icon: <AiOutlineInstagram/>},
+    {id: 2, link: 'https://github.com/kaiofsca', icon: <AiFillGithub/>},
+    {id: 3, link: 'https://www.linkedin.com/in/kaio-fonseca-8b8252221/', icon: <AiOutlineLinkedin/>}
+]
+
+export default data
