5e887a0df33177b8c3da54ff4d3ce76399d43e30
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-12-03 09:27:58 -0300

    add seção de sobre e estilizando

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,8 +1,42 @@
+import AboutImage from '../../assets/about.jpg'
+import CV from '../../assets/cv.pdf'
+import {HiDownload} from 'react-icons/hi'
 import './about.css'
+import data from './data'
+import Card from '../../components/Card'
 
 const About = () => {
   return (
-    <section id='about'><h2>About</h2></section>
+    <section id='about'>
+      <div className="conatiner about__container">
+        <div className="about__left">
+          <div className="about__portrait">
+            <img src={AboutImage} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__right">
+            <h2>About me</h2>
+            <div className="about__cards">
+              {
+                data.map(item => (
+                  <Card key={item.id} className="about__card">
+                    <span className='about__card-icon'>{item.icon}</span>
+                    <h5>{item.title}</h5>
+                    <small>{item.desc}</small>
+                  </Card>
+                ))
+              }
+            </div>
+            <p>
+            Tenho 19 anos, sou de Rio de Janeiro mas moro em Brasília e atualmente estou construindo conhecimentos sólidos em Front-End para em um futuro breve entrar no mercado.
+            </p>
+            <p>
+            Olá, meu nome é Kaio Marques Fonseca e me considero um cara que sempre procura resolver os problemas da melhor maneira e sempre procurando evoluir a cada dia. Desenvolvi meu primeiro site em 2021 utilizando HTML e CSS. Desde então venho aprimorando minhas habilidades com projetos cada vez mais desafiadores que você pode dar uma olhada no meu GitHub!
+            </p>
+            <a href={CV} download className='btn primary'>Download CV <HiDownload/></a>
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/sections/about/data.js
@@ -0,0 +1,11 @@
+import {FaAward} from 'react-icons/fa'
+import {TbBooks} from 'react-icons/tb'
+import {BiBookBookmark} from 'react-icons/bi'
+
+const data = [
+    {id: 1, icon: <FaAward/>, title:'Experience', desc:'1+ Years Studying'},
+    {id: 1, icon: <TbBooks/>, title:'Projects', desc:'40+ Completed'},
+    {id: 1, icon: <BiBookBookmark/>, title:'College', desc:'Third semester'}
+]
+
+export default data
