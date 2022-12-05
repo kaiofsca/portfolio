f2ec144e74695a349f162a5c0f668d6d048688ee
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-12-05 13:00:56 -0300

    add parte de skills

--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -1,8 +1,25 @@
+import Card from '../../components/Card'
+import data from './data'
 import './skills.css'
 
 const Skills = () => {
   return (
-    <section id='skills'><h2>Skills</h2></section>
+    <section id='skills'>
+      <h2>My Skills</h2>
+      <p>Aqui temos todas as minhas Hardskills</p>
+      <div className="container skills__container">
+        {
+          data.map(item => (
+            <Card key={item.id} className="skill light">
+              <div className="skill__icon">{item.icon}</div>
+              <div className="skill__details">
+                <h4>{item.title}</h4>
+              </div>
+            </Card>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ src/sections/skills/data.js
@@ -0,0 +1,35 @@
+import {AiFillHtml5} from 'react-icons/ai'
+import {DiCss3Full} from 'react-icons/di'
+import {SiJavascript} from 'react-icons/si'
+import {IoLogoSass} from 'react-icons/io'
+import {FaReact} from 'react-icons/fa'
+import {FaNodeJs} from 'react-icons/fa'
+import {SiFirebase} from 'react-icons/si'
+import {TbBrandNextjs} from 'react-icons/tb'
+import {SiRedux} from 'react-icons/si'
+import {AiTwotoneApi} from 'react-icons/ai'
+import {SiVite} from 'react-icons/si'
+import {SiStyledcomponents} from 'react-icons/si'
+import {BsGithub} from 'react-icons/bs'
+import {DiNpm} from 'react-icons/di'
+import {FaYarn} from 'react-icons/fa'
+
+const data = [
+    {id:1, icon: <AiFillHtml5/>, title: 'HTML'},
+    {id:2, icon: <DiCss3Full/>, title: 'CSS'},
+    {id:3, icon: <SiJavascript/>, title: 'JavaScript'},
+    {id:4, icon: <IoLogoSass/>, title: 'Sass'},
+    {id:5, icon: <FaReact/>, title: 'ReactJS'},
+    {id:6, icon: <FaNodeJs/>, title: 'NodeJS'},
+    {id:7, icon: <SiFirebase/>, title: 'Firebase'},
+    {id:8, icon: <TbBrandNextjs/>, title: 'NextJS'},
+    {id:9, icon: <SiRedux/>, title: 'Redux'},
+    {id:10, icon: <AiTwotoneApi/>, title: 'API'},
+    {id:11, icon: <SiVite/>, title: 'Vite'},
+    {id:12, icon: <SiStyledcomponents/>, title: 'Styled-component'},
+    {id:13, icon: <BsGithub/>, title: 'Git & GitHub'},
+    {id:14, icon: <DiNpm/>, title: 'NPM'},
+    {id:15, icon: <FaYarn/>, title: 'YARN'}
+]
+
+export default data
