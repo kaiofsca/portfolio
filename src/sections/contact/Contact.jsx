21ecc6984d195b81e66c9678c5d6cd57ebda6990
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-12-08 12:03:01 -0300

    add seção de contato

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,8 +1,22 @@
+import contacts from './data'
 import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'><h2>Contact</h2></section>
+    <section id='contact'>
+      <h2>Get in Touch</h2>
+      <p>
+        Shoot me a message via any of the links below!
+      </p>
+      <div className="container contact__container">
+        {
+          contacts.map(contact => 
+            <a key={contact.id} href={contact.link} target="_blank" rel='noopner noreferrer'>
+              {contact.icon}
+            </a>)
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ src/sections/contact/data.js
@@ -0,0 +1,11 @@
+import { FaWhatsapp } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi"
+
+const data = [
+    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:mkaiofonseca@gmail.com'},
+    {id: 2, icon: <FaLinkedin/>, link: 'https://www.linkedin.com/in/kaiofonseca/'},
+    {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/5521999050403'}
+]
+
+export default data
